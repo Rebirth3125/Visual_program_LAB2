62072a786dd909a2f37e2edf3ea2801849003497
Rebirth3125 <79037935+Rebirth3125@users.noreply.github.com>
2022-03-03 18:20:16 +0700

    Create Class2.cs

---
+++ Class2.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace VL2
+{
+    public class RomanNumberException : Exception
+    {
+        public RomanNumberException() : base() { }
+        public RomanNumberException(string message) : base(message) { }
+    }
+}
